47ba5b93775bd7c5c23e1eb632eeaca0e668b1ec
Jon Walls <8075115+ContrastingSounds@users.noreply.github.com>
2020-08-02 20:16:13 +0100

    strip out extension context

--- src/components/Extension.js
+++ src/components/Extension.js
@@ -1,11 +1,7 @@
-import React, { useContext } from 'react'
-import { ExtensionContext } from '@looker/extension-sdk-react'
-
+import React from 'react'
 import { Heading } from '@looker/components'
 
 class ExtensionInternal extends React.Component {
-  extensionContext = useContext(ExtensionContext)
-  
   render() {
     return (
       <Heading fontWeight='bold'>Simple Extension</Heading>
