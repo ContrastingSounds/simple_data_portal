e55a61cb9f3c8abc7c14a47884dfea8f8a0f7e9e
Jon Walls <8075115+ContrastingSounds@users.noreply.github.com>
2020-08-02 20:09:45 +0100

    added babel plugin for class properties

--- src/components/Extension.js
+++ src/components/Extension.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ExtensionContext } from '@looker/extension-sdk-react'
+
 import { Heading } from '@looker/components'
 
 class ExtensionInternal extends React.Component {
+  extensionContext = useContext(ExtensionContext)
+  
   render() {
     return (
       <Heading fontWeight='bold'>Simple Extension</Heading>
