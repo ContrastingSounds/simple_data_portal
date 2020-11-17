778a73123bbc946a659f2113774904bbf8f9f57d
Jon Walls <8075115+ContrastingSounds@users.noreply.github.com>
2020-11-17 15:21:00 +0000

    updated admin page

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -102,8 +102,8 @@ export const AdminPage = ({ canAdminister, config, updateConfig }) => {
           <Form m="small" onSubmit={updateConfiguration}>
             <Fieldset legend="Data Portal Configuration Options">
               <FieldText defaultValue={config.boardList} label="Default Boards (users can set their own using the Portal Boards attribute)" name="boardList" />
-              <InputColor defaultValue={config.color} name="color" />
-              <InputColor defaultValue={config.backgroundColor} name="backgroundColor" />
+              <InputColor defaultValue={config.color} label="text" name="color" />
+              <InputColor defaultValue={config.backgroundColor} label="background" name="backgroundColor" />
               <FieldText defaultValue={config.logoUrl} label="Logo URL" name="logoUrl" />
             </Fieldset>
             <Button type="submit">Update configuration</Button>
@@ -111,25 +111,26 @@ export const AdminPage = ({ canAdminister, config, updateConfig }) => {
         </FlexItem>
         <FlexItem>
           <Divider mt="medium" appearance="dark" />
-        </FlexItem>
-        <FlexItem>
           <Heading>Instructions</Heading>
           <Paragraph>
             The Data Portal is a light skin on top of your boards. A number of boards can be chosen to populate the portal. A single 
             set of boards can be used, stored as the default value for the portal_boards attribute. If a user or group has a different
             list of boards for their portal_boards user attribute, those will be used instead.
           </Paragraph>
-          <List type="bullet">
+          <Divider mt="medium" appearance="dark" />
+          <List type="number">
             <ListItem>Set a list of boards</ListItem>
             <ListItem>Set text and background colours</ListItem>
             <ListItem>Choose a logo</ListItem>
             <ListItem>On each board, set icons</ListItem>
           </List>
-        </FlexItem>
-        <FlexItem>
+          <Divider mt="medium" appearance="dark" />
           <Heading>Setting icons for dashboards and looks.</Heading>
           <Paragraph>
-            Each dashboard and look in a board can be given an icon for the sidebar.
+            Each dashboard and look in a board can be given an icon for the sidebar. To do so, for each section of the board, 
+            put the list of icons you would like to use in the description e.g. "Dashboard,DashboardGauge,ChartBar". The icons
+            are taken from the Looker Components library. The full list is here: 
+            <Link href="https://components.looker.com/components/content/icon/">https://components.looker.com/components/content/icon/</Link>
           </Paragraph>
         </FlexItem>
       </Flex>  
